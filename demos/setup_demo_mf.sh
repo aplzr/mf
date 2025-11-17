b073b030193b1f23a2f87a1fa945f1c88ea5df3a
ap <arnepelzerprivat@gmail.com>
2025-11-17 22:37:05 +0100

    Add demos

---
+++ demos/setup_demo_mf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if command -v mf &> /dev/null; then
+    rm $(mf cache file) 2>/dev/null || true
+    rm $(mf config file) 2>/dev/null || true
+fi
+
+uv tool uninstall mediafinder 2>/dev/null || true
+rm -rf ~/dummy_shows
+python ../src/mf/utils/generate_dummy_media.py ~
+mv -f ~/shows ~/dummy_shows
+mv -f ~/movies ~/dummy_movies
