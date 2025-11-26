87548397c9834dafff7714d3a9ef2dbde54c09f6
Arne Pelzer <arnepelzerprivat@gmail.com>
2025-11-26 11:51:07 +0100

    Add after_read hook to SettingSpec

--- src/mf/utils/settings.py
+++ src/mf/utils/settings.py
@@ -50,6 +50,7 @@ class SettingSpec:
         help: Human readable help text shown to the user.
         before_write: Hook to transform value(s) before persisting.
         after_update: Hook to trigger additional action(s) after an update.
+        after_read: Hook to transform value(s) after reading from config.
     """
 
     key: str
@@ -63,6 +64,7 @@ class SettingSpec:
     help: str = ""
     before_write: Callable[[Any], Any] = lambda value: value
     after_update: Callable[[Any], None] = lambda value: None
+    after_read: Callable[[Any], Any] = lambda value: value
 
 
 REGISTRY: dict[str, SettingSpec] = {
