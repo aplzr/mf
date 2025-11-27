f2594e9ec010ea2fc988259673bb5e43d598122f
ap <arnepelzerprivat@gmail.com>
2025-11-27 23:08:37 +0100

    Add cache stats command

--- src/mf/cli_cache.py
+++ src/mf/cli_cache.py
@@ -1,8 +1,9 @@
 import typer
 
-from .utils.cache import rebuild_library_cache
-from .utils.console import print_ok
+from .utils.cache import load_library_cache, rebuild_library_cache
+from .utils.console import console, print_ok
 from .utils.file import get_library_cache_file
+from .utils.stats import count_file_extensions, show_histogram
 
 app_cache = typer.Typer(help="Manage mf's library cache.")
 
@@ -24,3 +25,15 @@ def clear():
     """Clear the library cache."""
     get_library_cache_file().unlink()
     print_ok("Cleared the library cache.")
+
+
+@app_cache.command()
+def stats():
+    """Show cache statistics."""
+    # Extension histogram
+    cache = load_library_cache()
+    extension_counts = count_file_extensions(cache)
+    console.print("")
+    show_histogram(extension_counts, "File extension distribution")
+
+    # TODO: file size distribution
