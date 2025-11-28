2a42f4e54d7c182deabe323813ee66911a0f2954
ap <arnepelzerprivat@gmail.com>
2025-11-28 18:13:07 +0100

    Add resolution distribution to 'mf cache stats'

--- src/mf/cli_cache.py
+++ src/mf/cli_cache.py
@@ -3,6 +3,7 @@ import typer
 from .utils.cache import load_library_cache, rebuild_library_cache
 from .utils.console import console, print_ok
 from .utils.file import get_library_cache_file
+from .utils.parsers import parse_resolution
 from .utils.stats import count_file_extensions, show_histogram
 
 app_cache = typer.Typer(help="Manage mf's library cache.")
@@ -34,6 +35,9 @@ def stats():
     cache = load_library_cache()
     extension_counts = count_file_extensions(cache)
     console.print("")
-    show_histogram(extension_counts, "File extension distribution")
+    show_histogram(extension_counts, "Cache file extension distribution")
+
+    resolution_counts = parse_resolution(cache)
+    show_histogram(resolution_counts, "Cache file resolution distribution")
 
     # TODO: file size distribution
