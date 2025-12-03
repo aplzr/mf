b9a7c6b92c947cdfc2d4cd6b4d090cb79b4c4137
ap <arnepelzerprivat@gmail.com>
2025-12-03 23:13:13 +0100

    Update error message

--- src/mf/cli_main.py
+++ src/mf/cli_main.py
@@ -93,7 +93,7 @@ def play(
                 save_last_played(file_to_play)
             except ValueError as e:
                 print_and_raise(
-                    "Invalid target: {target}. Use an index number or 'next'.",
+                    "Invalid target: {target}. Use an index number, 'next', or 'list'.",
                     raise_from=e,
                 )
     else:
