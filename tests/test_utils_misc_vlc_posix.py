8503c428ae9b59cf4207280d382843f7b84cb2fb
ap <arnepelzerprivat@gmail.com>
2025-12-28 17:12:11 +0100

    Update test

--- tests/test_utils_misc_vlc_posix.py
+++ tests/test_utils_misc_vlc_posix.py
@@ -1,3 +1,4 @@
+import os
 from types import SimpleNamespace
 
 import mf.utils.play as play_mod
@@ -5,6 +6,8 @@ import mf.utils.play
 
 
 def test_get_vlc_command_posix(monkeypatch):
-    # Force POSIX branch
-    monkeypatch.setattr(play_mod, "os", SimpleNamespace(name="posix"))
+    # Force POSIX branch - need to include environ since get_vlc_command accesses it
+    monkeypatch.setattr(
+        play_mod, "os", SimpleNamespace(name="posix", environ=os.environ)
+    )
     assert mf.utils.play.get_vlc_command() == "vlc"
